435b0cf1b447586f096998a9f28556ee85f5acde
wedunty <107648024+wedunty@users.noreply.github.com>
2025-02-05 22:16:32 +0300

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,34 @@
+from datetime import datetime, timedelta
+
+def get_current_week_dates():
+    today = datetime.now()
+    start_of_week = today - timedelta(days=today.weekday())
+    week_dates = [(start_of_week + timedelta(days=i)).strftime("%d.%m.%Y") for i in range(7)]
+    return week_dates
+
+def get_attendants_list():
+    attendants = {}
+    with open("files/attendants_table.txt", "r", encoding="utf-8") as file:
+        attendants_table = list(map(lambda x: x.replace("\n", "").split(), file.readlines()))
+
+    for *info, date, state in attendants_table:
+        if state == "д":
+            attendants[date] = {"attendant": " ".join(info)}
+        elif state == "р":
+            attendants[date].update({"reserve_attendant": " ".join(info)})
+
+    return attendants
+
+def get_week_attendants_list(week_dates, attendants):
+    week_attendants = ""
+    for date in week_dates:
+        if date in attendants.keys():
+            week_attendants += f"<u><b>{date}</b></u>: <b>дежурный</b> - {attendants[date]["attendant"]} с <b>18:00</b> до <b>{(datetime.today() + timedelta(days=1)).strftime("%d.%m.%Y")} 9:00</b>, <b>резервный дежурный</b> - {attendants[date]["reserve_attendant"]}\n\n"
+        else:
+            break
+    return week_attendants
+
+def get_current_attendant(attendants):
+    date = datetime.today().strftime("%d.%m.%Y")
+    current_attendant = f"<u><b>{date}</b></u>: <b>дежурный</b> - {attendants[date]["attendant"]} с <b>18:00</b> до <b>{(datetime.today() + timedelta(days=1)).strftime("%d.%m.%Y")} 9:00</b>, <b>резервный дежурный</b> - {attendants[date]["reserve_attendant"]}"
+    return current_attendant
---
+++ main.py
@@ -0,0 +1,45 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.enums import ParseMode
+from aiogram.filters.command import Command
+from aiogram.client.default import DefaultBotProperties
+from datetime import datetime
+import functions
+from files.credentials import *
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(
+    token=BOT_TOKEN,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
+dp = Dispatcher()
+
+@dp.message(Command("week_attendants"))
+async def week_attendants(message: types.Message):
+    week_attendants =  functions.get_week_attendants_list(functions.get_current_week_dates(), functions.get_attendants_list())
+    await message.answer(f"<blockquote>{week_attendants}</blockquote>")
+
+@dp.message(Command("current_attendant"))
+async def current_attendant(message: types.Message):
+    current_attendant = functions.get_current_attendant(functions.get_attendants_list())
+    await message.answer(f"<blockquote>{current_attendant}</blockquote>")
+
+async def send_daily_notification():
+    while True:
+        today_hour, today_minute = datetime.today().hour, datetime.today().minute
+        if today_hour == 18 and today_minute == 00:
+            current_attendant = functions.get_current_attendant(functions.get_attendants_list())
+            await bot.send_message(CHAT_ID, f"<blockquote>{current_attendant}</blockquote>")
+            await asyncio.sleep(60)
+        await asyncio.sleep(1)
+    # await asyncio.sleep(1)
+
+async def main():
+    asyncio.create_task(send_daily_notification())
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+    # asyncio.create_task(send_daily_notification())
+
+if __name__ == "__main__":
+    asyncio.run(main())
