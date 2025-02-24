afda559945016f05066d3cb70162dcdd6f019297
wedunty <107648024+wedunty@users.noreply.github.com>
2025-02-24 21:05:58 +0300

    Add compatibility with multiple groups

--- functions.py
+++ functions.py
@@ -6,9 +6,9 @@ def get_current_week_dates():
     week_dates = [(start_of_week + timedelta(days=i)).strftime("%d.%m.%Y") for i in range(7)]
     return week_dates
 
-def get_attendants_list():
+def get_attendants_list(label):
     attendants = {}
-    with open("files/attendants_table.txt", "r", encoding="utf-8") as file:
+    with open(f"files/tables/{label}_table.txt", "r", encoding="utf-8") as file:
         attendants_table = list(map(lambda x: x.replace("\n", "").split(), file.readlines()))
 
     for *info, date, state in attendants_table:
@@ -38,3 +38,12 @@ def get_current_attendant(attendants):
     else:
         current_attendant = f"<u><b>{date}</b></u>: <b>дежурный</b> - {attendants[date]["attendant"]} с <b>18:00</b> до <b>{(datetime.strptime(date, "%d.%m.%Y").date() + timedelta(days=1)).strftime("%d.%m.%Y")} 9:00</b>\n\n"
     return current_attendant
+<<<<<<< HEAD
+
+def get_key(dict, value):
+    return list(dict.keys())[list(dict.values()).index(value)]
+
+
+=======
+
+>>>>>>> ccaedf7c771c997a5c166bafbbd3e98eef5861ec
